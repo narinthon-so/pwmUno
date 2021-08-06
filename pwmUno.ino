403e4f837ae3dc1173f452b3208be2af28fd1baf
narinthon-so <1997balebale@gmail.com>
2021-08-06 16:49:57 +0700

    first commit

--- pwmUno.ino
+++ pwmUno.ino
@@ -1,10 +1,12 @@
-#define ledPin 9  //กำหนด pin pwm สำหรับควบคุม led
+#define ledPin 5  //กำหนด pin pwm สำหรับควบคุม led
+#define ledPin2 6  //กำหนด pin pwm สำหรับควบคุม led
 
 void setup() {
   // initialize the serial communication:
   Serial.begin(115200);
   // initialize the ledPin as an output:
   pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 
   Serial.println("Input brightness value between 0-100%...");
 }
@@ -19,13 +21,14 @@ void loop() {
 
     Serial.println("Set brightness level to " + brightness);
 
-    // pwmVal = map(brightness.toInt(), 0, 100, 0, 255);
+//    pwmVal = map(brightness.toInt(), 0, 100, 0, 255);
 
-    pwmVal = (brightness.toInt() * 255) / 100;
+    pwmVal = (255 - (brightness.toInt() * 255) / 100);
 
-    //Serial.println(pwmVal);
+    Serial.println(pwmVal);
 
     // set the brightness of the LED:
     analogWrite(ledPin, pwmVal);
+    analogWrite(ledPin2, pwmVal);
   }
 }
